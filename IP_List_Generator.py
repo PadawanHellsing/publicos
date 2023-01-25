3ee32365db4c78507f7d5fb791a90dd872e30e8f
theycallmecharlie <38168227+theycallmecharlie@users.noreply.github.com>
2023-01-24 22:08:34 -0300

    Update IP_List_Generator.py
    Added usage of flags (output|range)

--- IP_List_Generator.py
+++ IP_List_Generator.py
@@ -1,11 +1,20 @@
 #!/bin/python
+import argparse
 from ipaddress import IPv4Network
 
-ip = input("Digite o IP e a rede que deseja utilizar (Formato: 192.168.0.0/24): ")
+def main():
+  parser = argparse.ArgumentParser(description="Gerador de IP's em um range")
+  parser.add_argument("-o", "--output", help="Nome do arquivo final", default="IPList.txt")
+  parser.add_argument("-r", "--range", help="Digite o IP e a rede que deseja utilizar (Formato: 192.168.0.0/24): ")
 
-for addr in IPv4Network(ip):
-  f = open("IPList.txt", "a")
-  f.write(f"{addr}\r\n")
-print('Lista salva em "IPList.txt".')
-f.close()
-exit
+  args = parser.parse_args()
+  generateRange(args.output,args.range)
+
+def generateRange(filename, range):
+  with open(filename, "w") as f: 
+    for ip in IPv4Network(range): f.write(f"{ip}\n")
+  print(f"Lista salva em {filename}.")
+
+if __name__ == "__main__":
+  try: main()
+  except Exception as err: print(f"Error:{err}")
